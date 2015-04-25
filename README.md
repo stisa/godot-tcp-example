848f7453d8c179ccf90376acb9ee49cdcd727b5c
Silvio T <stisa4@gmail.com>
2015-04-25 22:03:33 +0200

    fix receiving strings from node

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ So far I can read the data I send to node, and I can send back the same data.
 I currently have problems understanding the first 4 bytes of every TCP packet,
 so I can't send packets from node to godot without knowing those 4 bytes beforehand.
 
+Update: maybe I found what those first 4 bytes are: the length of the packet, without the first 4 bytes. So in a packet with 28bytes, the first 4bytes read '24', which is (28-4) the length of the packet!!
+
 TODO: Add references to the godot wiki about binary serialization, node wiki about buffer and net.
--- node/server.js
+++ node/server.js
@@ -3,18 +3,23 @@ var net = require('net')
 var client_count = 0
 
 var server = net.createServer(function(socket){
+	
 	client_count++
 	console.log('Client number %d connected', client_count)
 	socket.on('data',function(chunk){
+			
 			// First 4 bytes are ignored, the length of the header is 4bytes
 			// NOTE: If you need different kinds of inputs, you can use the integer
 			// value to differentiate data types
 			var chunkType = chunk.readUIntLE(4,4) 
+			
 			// In strings, the bytes from 4 to 7 (n. 4,5,6,7)
 			// are used to define the string length
 			var chunkLength = chunk.readUIntLE(8,4)
+			
 			// Length of the entire buffer
 			var chunkBufLength = chunk.length 
+			
 			// Actual string content starts on byte 12 
 			// and ends after %length of the string sent%
 			var chunkContent = chunk.toString('utf-8',12,12+chunkLength)
@@ -22,25 +27,28 @@ var server = net.createServer(function(socket){
 			// Let's see if he can reply
 			if (chunkContent=="who am I?"){
 			
-				var strResp = "you are a client"
+				var strResp = "you are a demo"
 				var strLength= Buffer.byteLength(strResp)
 				while (strLength%4){
 	
 					strLength++
 				}
+				
 				// The size of the buffer is:
-				// 4 bytes for the WTF bytes +
+				// 4 bytes for the length of the packet +
 				// 4 bytes for the type +
 				// 4 bytes for the length of the string +
 				// the length of the string, rounded up to the nearest multiple of 4
 				var response = new Buffer(4+4+4+strLength)
-				response.writeUIntLE(0x00000018,0,4) // WTF are those bytes???
+				response.writeUIntLE(4+4+strLength,0,4) // Write the length of the remaining packet
 				response.writeUIntLE(0x00000004,4,4) // Write the type, 4 is for strings
 				response.writeUIntLE(Buffer.byteLength(strResp),8,4) // Write the length of the string (in bytes)
 				response.write(strResp,12) // Write the actual string
-				console.log("strlength %d",response.length)
+				
 				socket.write(response)
-				console.log(response)
+				
+				//console.log("strlength %d",response.length)
+				//console.log(response)
 			
 			} else {
 			
@@ -50,6 +58,7 @@ var server = net.createServer(function(socket){
 				console.log("String length: "+chunkLength)
 				console.log("Read: "+chunkContent)
 				console.log(chunk)
+				
 				// Send the data back to the client to verify that
 				// the communication works both ways.
 				// NOTE: Remember to send the encoded data
